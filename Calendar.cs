3dd241c3c049fb561fc3b2382077a6013b493878
atul <amirajka@epic.com>
2021-05-04 14:28:53 -0500

    initial commit

---
+++ Calendar.cs
@@ -0,0 +1,119 @@
+﻿//----------------------------------------------------
+// Copyright 2021 Epic Systems Corporation
+//----------------------------------------------------
+
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace cowin_poller
+{
+    public partial class Calendar
+    {
+        [JsonPropertyName("centers")]
+        public List<Center> Centers { get; set; }
+    }
+
+    public class Center : IComparable<Center>
+    {
+        [JsonPropertyName("center_id")]
+        public long CenterId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("name_l")]
+        public string NameL { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("address_l")]
+        public string AddressL { get; set; }
+
+        [JsonPropertyName("state_name")]
+        public string StateName { get; set; }
+
+        [JsonPropertyName("state_name_l")]
+        public string StateNameL { get; set; }
+
+        [JsonPropertyName("district_name")]
+        public string DistrictName { get; set; }
+
+        [JsonPropertyName("district_name_l")]
+        public string DistrictNameL { get; set; }
+
+        [JsonPropertyName("block_name")]
+        public string BlockName { get; set; }
+
+        [JsonPropertyName("block_name_l")]
+        public string BlockNameL { get; set; }
+
+        [JsonPropertyName("pincode")]
+        public int Pincode { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+
+        [JsonPropertyName("long")]
+        public double Long { get; set; }
+
+        [JsonPropertyName("from")]
+        public DateTimeOffset From { get; set; }
+
+        [JsonPropertyName("to")]
+        public DateTimeOffset To { get; set; }
+
+        [JsonPropertyName("fee_type")]
+        public string FeeType { get; set; }
+
+        [JsonPropertyName("vaccine_fees")]
+        public List<VaccineFee> VaccineFees { get; set; }
+
+        [JsonPropertyName("sessions")]
+        public List<SessionCalendar> Sessions { get; set; }
+
+        public double interestedLat { get; set; }
+        public double interestedLng { get; set; }
+        public int CompareTo([AllowNull] Center other)
+        {
+            if (other == null) return 1;
+
+            var thisDistance = Math.Sqrt(((this.Lat - interestedLat) * (this.Lat - interestedLat)) + ((this.Long - interestedLng) * (this.Long - interestedLng)));
+            var otherDistance = Math.Sqrt(((other.Lat - interestedLat) * (other.Lat - interestedLat)) + ((other.Long - interestedLng) * (other.Long - interestedLng)));
+            return thisDistance.CompareTo(otherDistance);
+        }
+    }
+
+    public class SessionCalendar
+    {
+        [JsonPropertyName("session_id")]
+        public Guid SessionId { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("available_capacity")]
+        public long AvailableCapacity { get; set; }
+
+        [JsonPropertyName("min_age_limit")]
+        public long MinAgeLimit { get; set; }
+
+        [JsonPropertyName("vaccine")]
+        public string Vaccine { get; set; }
+
+        [JsonPropertyName("slots")]
+        public List<string> Slots { get; set; }
+
+    }
+
+    public partial class VaccineFee
+    {
+        [JsonPropertyName("vaccine")]
+        public string Vaccine { get; set; }
+
+        [JsonPropertyName("fee")]
+        public decimal Fee { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿//----------------------------------------------------
+// Copyright 2021 Epic Systems Corporation
+//----------------------------------------------------
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cowin_poller
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string baseURL = "https://cdn-api.co-vin.in/api/v2/";
+        public Program()
+        {
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+        }
+
+        static async Task Main(string[] args)
+        {
+            //https://github.com/bhattbhavesh91/cowin-vaccination-slot-availability/blob/main/district_mapping.csv
+            //Poll("atulmirajkar@gmail.com", 363, 18.65, 73.8, 44, 15);
+            await ProcessRequest("atulmirajkar@gmail.com", 363, 18.65, 73.8, 44);
+        }
+
+        private static void Poll(string email, int districtID, double interestedLat, double interestedLng, int minAgeLimit, int minutes = 15)
+        {
+            var timer = new Timer(async (obj) => await ProcessRequest(email, districtID, interestedLat, interestedLng, minAgeLimit),null,0,minutes*60*1000);
+        }
+
+        private static async Task ProcessRequest(string email, int districtID, double interestedLat, double interestedLng, int minAgeLimit)
+        {
+            if (email == "" || districtID < 1) return;
+
+            var streamCalendar = client.GetStreamAsync(baseURL + "appointment/sessions/public/calendarByDistrict?district_id=" + districtID + "&date=" + getTodaysDate());
+            var calendar = await JsonSerializer.DeserializeAsync<Calendar>(await streamCalendar);
+
+            //sort calendars by interested lat and interested long
+            foreach(Center center in calendar.Centers)
+            {
+                center.interestedLat = interestedLat;
+                center.interestedLng = interestedLng; 
+            }
+            calendar.Centers.Sort();
+            var htmlContent = createContent(calendar, minAgeLimit);
+            await sendEmail(email,htmlContent);
+        }
+
+        private static async Task sendEmail(string email, string htmlContent)
+        {
+            //https://sendgrid.com/docs/for-developers/sending-email/v3-csharp-code-example/
+            //https://github.com/sendgrid/sendgrid-csharp/#installation
+            var client = new SendGridClient("");    //todo
+            var from = new EmailAddress("atulmirajkar@gmail.com", "atul");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlContent);
+            await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+
+        private static string createContent(Calendar calendar, int minAgeLimit)
+        {
+            string result = "";
+            
+            foreach(Center center in calendar.Centers)
+            {
+                string sessionStr = "";
+                foreach (SessionCalendar sessionCal in center.Sessions)
+                {
+                    if (sessionCal.MinAgeLimit < minAgeLimit) continue;
+
+                    sessionStr += $"<div>Date: {sessionCal.Date}</div>";
+                    sessionStr += $"<div>Min Age: {sessionCal.MinAgeLimit}</div>";
+                    sessionStr += $"<div>Vaccine: {sessionCal.Vaccine}</div>";
+                    sessionStr += $"<div>Slots:</div>";
+                    foreach (string slot in sessionCal.Slots)
+                    {
+                        sessionStr += $"<div>{slot}</div>";
+                    }
+                }
+
+                if (sessionStr == "") continue;
+
+
+                string centerStr = "";
+                centerStr = centerStr + $"<h1>{center.Name}</h1>";
+                centerStr = centerStr + $"<h2>{center.Address}</h2>";
+                centerStr = centerStr + $"<h2>{center.DistrictName}</h2>";
+                centerStr = centerStr + $"<h2>{center.BlockName}</h2>";
+                centerStr = centerStr + $"<h2>{center.Pincode}</h2>";
+
+                centerStr = centerStr + $"<div>Fee Type: {center.FeeType}</div>";
+                foreach(VaccineFee vaccineFee in center.VaccineFees)
+                {
+                    centerStr = centerStr + $"<div>Vaccine: {vaccineFee.Vaccine}. Fee: {vaccineFee.Fee}</div>";
+                }
+
+                result += centerStr + sessionStr;
+            }
+
+            return result;
+        }
+
+    
+
+        private static string getTodaysDate()
+        {
+            DateTime date = DateTime.Today;
+            IFormatProvider culture = new System.Globalization.CultureInfo("hi-IN", true);
+            return date.GetDateTimeFormats(culture)[0];
+        }
+    }
+}
